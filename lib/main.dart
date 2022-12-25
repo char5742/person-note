6b963eb2ec89be1c064f8f2673c0048d0a866770
char5742 <f.shuuma@gmail.com>
2022-12-25 17:13:57 +0900

    doc: add firebase info

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
 import 'app.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const ProviderScope(child: App()));
 }
