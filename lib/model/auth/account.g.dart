ae4427b91129ce33098b952e64ed0caf1b83a5d2
char5742 <f.shuuma@gmail.com>
2022-12-28 19:04:19 +0900

    #6 feat: add Account model

---
+++ lib/model/auth/account.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'account.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Account _$$_AccountFromJson(Map<String, dynamic> json) => _$_Account(
+      uid: json['uid'] as String,
+      displayName: json['displayName'] as String,
+    );
+
+Map<String, dynamic> _$$_AccountToJson(_$_Account instance) =>
+    <String, dynamic>{
+      'uid': instance.uid,
+      'displayName': instance.displayName,
+    };
