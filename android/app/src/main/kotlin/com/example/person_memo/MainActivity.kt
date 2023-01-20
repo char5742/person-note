4b41cd406214268f73582db4cd6cc31e3d0766d3
Char☆ <50830649+char5742@users.noreply.github.com>
2023-01-20 23:26:10 +0900

    change app name (#34)

--- android/app/src/main/kotlin/com/example/person_memo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/person_memo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.person_memo
+package com.example.person_note
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:person_memo/main.dart';
+import 'package:person_note/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
