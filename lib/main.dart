3ac4cd3cd6f456e1750b219983b226961a2f952d
Char☆ <50830649+char5742@users.noreply.github.com>
2023-01-24 17:59:36 +0900

    localeが正常に切り替わるように (#49)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:person_note/usecase/auth.dart';
 
 import 'firebase_options.dart';
@@ -11,7 +12,8 @@ import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting(Platform.localeName);
+  Intl.defaultLocale = Platform.localeName;
+  await initializeDateFormatting();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
