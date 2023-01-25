edd711fedacd695c761d583ad2d3ccf4d01d32f9
Char☆ <50830649+char5742@users.noreply.github.com>
2023-01-25 13:42:10 +0900

    component切り出し (#60)

--- lib/page/component.dart
+++ lib/page/component.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:person_note/util/date_format.dart';
+import 'package:person_note/util/validator.dart';
 
 class CirclePersonIconBox extends StatelessWidget {
   final double? size;
@@ -45,5 +49,139 @@ class CustomMonthDayPicker extends DatePickerModel {
   @override
   List<int> layoutProportions() {
     return [0, 1, 1];
+  }
+}
+
+class PersonForm extends HookConsumerWidget {
+  final Key formKey;
+  final TextEditingController nameController;
+  final TextEditingController ageController;
+  final TextEditingController emailController;
+  final TextEditingController memoController;
+  final ValueNotifier<DateTime?> birthday;
+  final ValueNotifier<List<String>> tags;
+
+  const PersonForm({
+    super.key,
+    required this.formKey,
+    required this.nameController,
+    required this.ageController,
+    required this.emailController,
+    required this.memoController,
+    required this.birthday,
+    required this.tags,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                validator: Validator.isNonNullString,
+                decoration: const InputDecoration(label: Text('Name')),
+              ),
+              TextFormField(
+                controller: ageController,
+                validator: Validator.isNumber,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(label: Text('Age')),
+              ),
+              TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(label: Text('E-mail')),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Row(
+                  children: [
+                    Text('Birthday', style: theme.textTheme.labelLarge),
+                    const Padding(padding: EdgeInsets.only(left: 16.0)),
+                    Text(
+                      formatDate(birthday.value),
+                      style: theme.textTheme.bodyLarge,
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        final date = await DatePicker.showPicker(
+                          context,
+                          pickerModel:
+                              CustomMonthDayPicker(currentTime: DateTime(0)),
+                        );
+                        birthday.value = date;
+                      },
+                      icon: const Icon(Icons.date_range),
+                    ),
+                  ],
+                ),
+              ),
+              TextFormField(
+                controller: memoController,
+                maxLines: null,
+                decoration: const InputDecoration(label: Text('Memo')),
+              ),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    Text('Tags', style: theme.textTheme.labelLarge),
+                    ...tags.value.map((e) => Card(
+                            child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0,
+                            vertical: 4.0,
+                          ),
+                          child: Text(
+                            e,
+                            style: theme.textTheme.bodyLarge,
+                          ),
+                        ))),
+                    IconButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            final controller = TextEditingController();
+                            return SimpleDialog(
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 16.0,
+                                vertical: 8.0,
+                              ),
+                              children: [
+                                TextFormField(controller: controller),
+                                IconButton(
+                                  onPressed: () {
+                                    tags.value = [
+                                      ...tags.value,
+                                      controller.text
+                                    ];
+                                    context.pop();
+                                  },
+                                  icon: const Icon(Icons.add),
+                                )
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      icon: const Icon(Icons.add),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/page/create.dart
+++ lib/page/create.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:person_note/model/person/person.dart';
 import 'package:person_note/provider/person.dart';
-import 'package:person_note/util/date_format.dart';
-import 'package:person_note/util/validator.dart';
 
 import 'component.dart';
 
@@ -16,7 +13,6 @@ class CreatePage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isProcessing = useState(false);
     final formKey = useMemoized(GlobalKey<FormState>.new, const []);
-    final theme = Theme.of(context);
     final nameConteroller = useTextEditingController();
     final ageConteroller = useTextEditingController();
     final emailConteroller = useTextEditingController();
@@ -53,112 +49,14 @@ class CreatePage extends HookConsumerWidget {
           ),
         ],
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: nameConteroller,
-                  validator: Validator.isNonNullString,
-                  decoration: const InputDecoration(label: Text('Name')),
-                ),
-                TextFormField(
-                  controller: ageConteroller,
-                  validator: Validator.isNumber,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(label: Text('Age')),
-                ),
-                TextFormField(
-                  controller: emailConteroller,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(label: Text('E-mail')),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Row(
-                    children: [
-                      Text('Birthday', style: theme.textTheme.labelLarge),
-                      const Padding(padding: EdgeInsets.only(left: 16.0)),
-                      Text(
-                        formatDate(birthday.value),
-                        style: theme.textTheme.bodyLarge,
-                      ),
-                      IconButton(
-                        onPressed: () async {
-                          final date = await DatePicker.showPicker(
-                            context,
-                            pickerModel:
-                                CustomMonthDayPicker(currentTime: DateTime(0)),
-                          );
-                          birthday.value = date;
-                        },
-                        icon: const Icon(Icons.date_range),
-                      ),
-                    ],
-                  ),
-                ),
-                TextFormField(
-                  controller: memoConteroller,
-                  decoration: const InputDecoration(label: Text('Memo')),
-                ),
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    children: [
-                      Text('Tags', style: theme.textTheme.labelLarge),
-                      ...tags.value.map((e) => Card(
-                              child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0,
-                              vertical: 4.0,
-                            ),
-                            child: Text(
-                              e,
-                              style: theme.textTheme.bodyLarge,
-                            ),
-                          ))),
-                      IconButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              final controller = TextEditingController();
-                              return SimpleDialog(
-                                contentPadding: const EdgeInsets.symmetric(
-                                  horizontal: 16.0,
-                                  vertical: 8.0,
-                                ),
-                                children: [
-                                  TextFormField(controller: controller),
-                                  IconButton(
-                                    onPressed: () {
-                                      tags.value = [
-                                        ...tags.value,
-                                        controller.text
-                                      ];
-                                      context.pop();
-                                    },
-                                    icon: const Icon(Icons.add),
-                                  )
-                                ],
-                              );
-                            },
-                          );
-                        },
-                        icon: const Icon(Icons.add),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+      body: PersonForm(
+        formKey: formKey,
+        nameController: nameConteroller,
+        ageController: ageConteroller,
+        emailController: emailConteroller,
+        memoController: memoConteroller,
+        birthday: birthday,
+        tags: tags,
       ),
     );
   }
